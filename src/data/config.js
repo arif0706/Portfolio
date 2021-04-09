481ffc262e5eb3ef647b45c8789e4092e5a6b89c
arif0706 <mohammedarif0706@gmail.com>
2021-04-09 10:51:24 +0530

    first commit

---
+++ src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Mohammed Arif",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Mohammed Arif and I'm an Android Developer!",
+  githubUrl: "https://github.com/arif0706",
+  linkedinUrl: "",
+  cvLink: "",
+};
